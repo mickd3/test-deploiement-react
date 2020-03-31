8c71451fbda95ca081c6445ed78efabc5684f965
Mickd3 <mickaeld_03@hotmail.fr>
2020-03-31 14:12:36 +0200

    first commit

---
+++ src/reducers/add.js
@@ -0,0 +1,24 @@
+let initialState = {
+}
+
+const AddReducer = (state = initialState, action) => {
+
+    switch (action.type) {
+        case 'ADD_ONE':
+            state = {
+                ...state,
+                value: state.value + 1
+            }
+            return state;
+        case 'ADD_X':
+            state = {
+                ...state,
+                value: state.value + action.payload
+            }
+            return state;
+        default:
+            return state;
+    }
+}
+
+export default AddReducer
